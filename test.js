0008a3fa817aaa92bda9a50e325f3acaad811b65
lukeschunk <luke.schunk@mx.com>
2017-01-01 15:41:56 -0700

    rain first cut

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-
+//
 var synthNoSidechain = new Tone.Player("./loops/synth-pad-naked.mp3").toMaster();
 synthNoSidechain.loop = true;
 
@@ -10,9 +10,17 @@ kick.loop = true;
 var drums = new Tone.Player("./loops/drums.mp3").toMaster();
 drums.loop = true;
 
+var bass = new Tone.Player("./loops/bass.mp3").toMaster();
+bass.loop = true;
+
+var rain = new Tone.Player("./loops/rain.mp3").toMaster();
+rain.loop = true;
+
 Tone.Buffer.on('load', function(){
-    synthNoSidechain.start();
-    synth.start();
-    kick.start();
-    drums.start();
+    // synthNoSidechain.start();
+    // synth.start();
+    // kick.start();
+    // drums.start();
+    // rain.start();
+    // bass.start();
 })
